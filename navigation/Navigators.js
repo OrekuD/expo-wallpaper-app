d05d4d25336f0a3895c26ebfc991ca4fec6c5914
unknown <opokudavid446@gmail.com>
2020-06-05 23:06:15 +0100

    Completed image preview

--- navigation/Navigators.js
+++ navigation/Navigators.js
@@ -12,99 +12,7 @@ import Art from "../screens/Art";
 import Nature from "../screens/Nature";
 
 import ImagePreview from "../screens/ImagePreview";
-
-import { View, TouchableOpacity } from "react-native";
-import Animated from "react-native-reanimated";
 import Constants from "expo-constants";
-
-const marginTop = new Animated.Value(1);
-const config = {
-  damping: 15,
-  mass: 1,
-  stiffness: 200,
-  overshootClamping: false,
-  restSpeedThreshold: 0.001,
-  restDisplacementThreshold: 0.001,
-};
-
-function MyTabBar({ state, descriptors, navigation, position }) {
-  return (
-    <Animated.View
-      style={{
-        flexDirection: "row",
-        height: Constants.statusBarHeight + 60,
-        alignItems: "flex-end",
-      }}
-    >
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
-
-        const isFocused = state.index === index;
-
-        const onPress = () => {
-          const event = navigation.emit({
-            type: "tabPress",
-            target: route.key,
-            canPreventDefault: true,
-          });
-
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
-
-        const onLongPress = () => {
-          navigation.emit({
-            type: "tabLongPress",
-            target: route.key,
-          });
-        };
-
-        const inputRange = state.routes.map((_, i) => i);
-        const animation = Animated.interpolate(position, {
-          inputRange,
-          outputRange: inputRange.map((i) => (i === index ? 1 : 0)),
-        });
-
-        const fontSize = Animated.interpolate(position, {
-          inputRange,
-          outputRange: inputRange.map((i) => (i === index ? 32 : 24)),
-        });
-
-        return (
-          <TouchableOpacity
-            accessibilityRole="button"
-            accessibilityStates={isFocused ? ["selected"] : []}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={{ flex: 1, alignItems: "center" }}
-            key={Math.random().toString()}
-          >
-            <Animated.Text
-              style={{
-                fontSize,
-                opacity: 1,
-                width: 160,
-                textAlign: "center",
-                fontWeight: isFocused ? "bold" : "100",
-              }}
-            >
-              {label}
-            </Animated.Text>
-          </TouchableOpacity>
-        );
-      })}
-    </Animated.View>
-  );
-}
 
 const TopTab = createMaterialTopTabNavigator();
 const TopTabNavigator = () => {
@@ -137,10 +45,54 @@ const StackScreen = () => {
   return (
     <Stack.Navigator headerMode="none">
       <Stack.Screen name="Categories" component={Categories} />
-      <Stack.Screen name="Pets" component={Pets} />
-      <Stack.Screen name="Nature" component={Nature} />
+      <Stack.Screen name="Pets" component={PetsSharedStackScreen} />
+      <Stack.Screen name="Nature" component={NatureSharedStackScreen} />
       <Stack.Screen name="Art" component={Art} />
     </Stack.Navigator>
+  );
+};
+
+const PetsSharedStack = createSharedElementStackNavigator();
+const PetsSharedStackScreen = () => {
+  return (
+    <PetsSharedStack.Navigator
+      initialRouteName="Pets"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <PetsSharedStack.Screen name="Pets" component={Pets} />
+      <PetsSharedStack.Screen
+        name="ImagePreview"
+        component={ImagePreview}
+        sharedElementsConfig={(route, otherRoute, showing) => {
+          const { item } = route.params;
+          return [`item.${item.id}.photo`];
+        }}
+      />
+    </PetsSharedStack.Navigator>
+  );
+};
+
+const NatureSharedStack = createSharedElementStackNavigator();
+const NatureSharedStackScreen = () => {
+  return (
+    <NatureSharedStack.Navigator
+      initialRouteName="Nature"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <NatureSharedStack.Screen name="Nature" component={Nature} />
+      <NatureSharedStack.Screen
+        name="ImagePreview"
+        component={ImagePreview}
+        sharedElementsConfig={(route, otherRoute, showing) => {
+          const { item } = route.params;
+          return [`item.${item.id}.photo`];
+        }}
+      />
+    </NatureSharedStack.Navigator>
   );
 };
 
--- screens/ImagePreview.js
+++ screens/ImagePreview.js
@@ -1,19 +1,106 @@
 import React, { useEffect, useContext } from "react";
-import { View, StyleSheet, Dimensions, Text, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Text,
+  Image,
+  TouchableOpacity,
+  Share,
+  Alert,
+} from "react-native";
 import { SharedElement } from "react-navigation-shared-element";
 import { height, width } from "../constants/Layout";
 import { Context } from "../context/context";
+import Animated from "react-native-reanimated";
+import { Ionicons, Feather } from "@expo/vector-icons";
+import * as Permissions from "expo-permissions";
+import * as FileSystem from "expo-file-system";
+import * as MediaLibrary from "expo-media-library";
 
 const ImagePreview = (props) => {
   const { toggleSwipe } = useContext(Context);
   const { item } = props.route.params;
 
+  const translateY = new Animated.Value(0);
+  const opacity = translateY.interpolate({
+    inputRange: [0, 100],
+    outputRange: [1, 0],
+  });
+
+  const config = {
+    damping: 15,
+    mass: 1,
+    stiffness: 200,
+    overshootClamping: false,
+    restSpeedThreshold: 0.001,
+    restDisplacementThreshold: 0.001,
+  };
+
+  const Anim = () => {
+    Animated.spring(translateY, {
+      toValue: 0,
+      ...config,
+    }).start();
+  };
+
+  const shareImageDetails = async () => {
+    try {
+      const result = await Share.share({
+        message: `I think you would love this image. Check it out! \n ${item.pageURL}`,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          console.log("Trying");
+        } else {
+          console.log("Succesfully shared");
+        }
+      } else if (result.action === Share.dismissedAction) {
+        console.log("Dimissed");
+      }
+    } catch (error) {
+      Alert.alert("Share", "There was an issue sharing your image");
+    }
+  };
+
+  const downloadImage = async () => {
+    let cameraPermissions = await Permissions.getAsync(Permissions.CAMERA);
+    if (cameraPermissions.status !== "granted") {
+      cameraPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    }
+    if (cameraPermissions.status === "granted") {
+      FileSystem.downloadAsync(
+        item.largeImageURL,
+        FileSystem.documentDirectory + item.id + ".jpg"
+      )
+        .then(async ({ uri }) => {
+          const asset = await MediaLibrary.createAssetAsync(uri);
+          MediaLibrary.createAlbumAsync("Wallpapers", asset)
+            .then(() => {
+              Alert.alert("Success", "Image has been saved");
+              FileSystem.deleteAsync(uri);
+            })
+            .catch((error) => {});
+        })
+        .catch((error) => Alert.alert("Error", "Could not save image"));
+    }
+  };
+
   useEffect(() => {
     toggleSwipe(0);
+    setTimeout(() => {
+      Anim();
+    }, 500);
+
     return () => {
       toggleSwipe(1);
     };
   }, []);
+
+  // const opacity = translateY.interpolate({
+  //   inputRange: [-100, 0],
+  //   outputRange: [0, 1],
+  // });
 
   return (
     <View style={styles.container}>
@@ -32,6 +119,24 @@ const ImagePreview = (props) => {
           resizeMode="cover"
         />
       </SharedElement>
+      <Animated.View
+        style={{ ...styles.buttons, transform: [{ translateY }], opacity }}
+      >
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.button}
+          onPress={shareImageDetails}
+        >
+          <Feather name="share" color="white" size={24} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.button}
+          onPress={downloadImage}
+        >
+          <Feather name="download" color="white" size={26} />
+        </TouchableOpacity>
+      </Animated.View>
     </View>
   );
 };
@@ -39,6 +144,26 @@ const ImagePreview = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  buttons: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    width: width,
+    height: 100,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: width > 400 ? 130 : 100,
+    zIndex: 10,
+  },
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: "rgba(1, 1, 1, 0.6)",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
