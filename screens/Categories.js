378d375ea0d5b9c2536256ee0e16afc34825f030
unknown <opokudavid446@gmail.com>
2020-06-06 11:32:06 +0100

    Changes to categories layout

--- screens/Categories.js
+++ screens/Categories.js
@@ -5,53 +5,42 @@ import CategoryCard from "../components/CategoryCard";
 
 const { width, height } = Dimensions.get("screen");
 
-const firstCategory = [
-  [
-    {
-      key: Math.random().toString(),
-      name: "Art",
-      image: require("../assets/images/art.jpg"),
-    },
-    {
-      key: Math.random().toString(),
-      name: "Nature",
-      image: require("../assets/images/nature.jpg"),
-    },
-  ],
-  [
-    {
-      key: Math.random().toString(),
-      name: "Food",
-      image: require("../assets/images/food.jpg"),
-    },
-    {
-      key: Math.random().toString(),
-      name: "Travel",
-      image: require("../assets/images/travel.jpg"),
-    },
-  ],
-];
-
-const secondCategory = [
-  [
-    {
-      key: Math.random().toString(),
-      name: "Music",
-      image: require("../assets/images/music.jpg"),
-    },
-  ],
-  [
-    {
-      key: Math.random().toString(),
-      name: "Buildings",
-      image: require("../assets/images/buildings.jpg"),
-    },
-    {
-      key: Math.random().toString(),
-      name: "Pets",
-      image: require("../assets/images/pet-1.jpg"),
-    },
-  ],
+const categoriesDetails = [
+  {
+    key: Math.random().toString(),
+    name: "Music",
+    image: require("../assets/images/music.jpg"),
+  },
+  {
+    key: Math.random().toString(),
+    name: "Art",
+    image: require("../assets/images/art.jpg"),
+  },
+  {
+    key: Math.random().toString(),
+    name: "Pets",
+    image: require("../assets/images/pet-1.jpg"),
+  },
+  {
+    key: Math.random().toString(),
+    name: "Nature",
+    image: require("../assets/images/nature.jpg"),
+  },
+  {
+    key: Math.random().toString(),
+    name: "Food",
+    image: require("../assets/images/food.jpg"),
+  },
+  {
+    key: Math.random().toString(),
+    name: "Travel",
+    image: require("../assets/images/travel.jpg"),
+  },
+  {
+    key: Math.random().toString(),
+    name: "Buildings",
+    image: require("../assets/images/buildings.jpg"),
+  },
 ];
 
 const Categories = ({ navigation }) => {
@@ -59,48 +48,14 @@ const Categories = ({ navigation }) => {
     navigation.navigate(name);
   };
 
-  const renderHeader = () => (
-    <FlatList
-      data={secondCategory[0]}
-      renderItem={({ item }) => (
-        <CategoryCard full item={item} onPress={viewCategory} />
-      )}
-    />
-  );
-
-  const renderFooter = () => (
-    <FlatList
-      data={secondCategory[1]}
-      renderItem={({ item }) => (
-        <CategoryCard full item={item} onPress={viewCategory} />
-      )}
-      ListFooterComponent={() => (
-        <FlatList
-          data={firstCategory[1]}
-          numColumns={2}
-          keyExtractor={() => Math.random().toString()}
-          renderItem={({ item }) => (
-            <CategoryCard item={item} onPress={viewCategory} />
-          )}
-          columnWrapperStyle={styles.row}
-        />
-      )}
-    />
-  );
-
   return (
     <View style={styles.container}>
       <FlatList
-        data={firstCategory[0]}
-        numColumns={2}
-        columnWrapperStyle={styles.row}
+        data={categoriesDetails}
         keyExtractor={() => Math.random().toString()}
-        ListHeaderComponent={renderHeader}
-        ListHeaderComponentStyle={styles.row}
         renderItem={({ item }) => (
-          <CategoryCard item={item} onPress={viewCategory} />
+          <CategoryCard full item={item} onPress={viewCategory} />
         )}
-        ListFooterComponent={renderFooter}
       />
     </View>
   );
